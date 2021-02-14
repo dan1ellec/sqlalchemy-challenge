789d3033c43d005c9762d29c5add1af1b6b509dd
Danielle C <72367042+dan1ellec@users.noreply.github.com>
2021-02-14 18:25:53 +1100

    added station api

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # Imports
 
 import numpy as np
+import datetime as dt
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
@@ -61,9 +62,10 @@ def precipitation():
         filter(Measurement.date >= year_ago).\
         order_by(Measurement.date).all()
 
+    # Closing the session
     session.close
 
-    # detailing the last year of rain data with date/prcp dictionary
+    # Detailing the last year of rain data with date/prcp dictionary
     date_prcp = []
     for date, prcp in results:
         prcp_dict = {}
@@ -71,8 +73,28 @@ def precipitation():
         prcp_dict["prcp"] = prcp
         date_prcp.append(prcp_dict)
 
-    return jsonify(all_passengers)
+    return jsonify(date_prcp)
 
+# Stations route
+@app.route("/api/v1.0/stations")
+def stations():
+    # Creating our session (link) from Python to the DB
+    session = Session(engine)
+
+    stations = session.query(Station.station, Station.name)
+
+    # Closing the session
+    session.close
+
+    # Return a JSON list of stations from the dataset.
+    station_id_name = []
+    for station, name in stations:
+        station_dict = {}
+        station_dict["station id"] = station
+        station_dict["station name"] = name
+        station_id_name.append(station_dict)
+
+    return jsonify(station_id_name)
 
 
 if __name__ == '__main__':
