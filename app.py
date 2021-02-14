e3a2d3b373df73b2ccea829d06b98a5fd1387e96
Danielle C <72367042+dan1ellec@users.noreply.github.com>
2021-02-14 17:48:50 +1100

    prcp section app.py

--- app.py
+++ app.py
@@ -28,3 +28,53 @@ Station = Base.classes.station
 # Flask Setup
 #################################################
 app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+# Home route listing all available api routes
+@app.route("/")
+def welcome():
+    return (
+        f"Welcome to the Surfs Up Climate API"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end><br/>"
+        
+    )
+
+
+# Precipitation route
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Creating our session (link) from Python to the DB
+    session = Session(engine)
+
+    year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+
+    results = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= year_ago).\
+        order_by(Measurement.date).all()
+
+    session.close
+
+    # detailing the last year of rain data with date/prcp dictionary
+    date_prcp = []
+    for date, prcp in results:
+        prcp_dict = {}
+        prcp_dict["date"] = date
+        prcp_dict["prcp"] = prcp
+        date_prcp.append(prcp_dict)
+
+    return jsonify(all_passengers)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
